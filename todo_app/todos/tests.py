1d38b6d5219f3ee563c81723d3edaecc81c3bbc6
AHKAYY007 <iamakwaubokemeh@gmail.com>
2025-09-14 01:49:26 +0100

    DRF project2: Todo API with heroku deployment setup

---
+++ todo_app/todos/tests.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from .models import Todo
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+# Create your tests here.
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.todo = Todo.objects.create(
+            title='First Todo',
+            body='Text body goes here'
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, 'First Todo')
+        self.assertEqual(self.todo.body, 'Text body goes here')
+        self.assertEqual(str(self.todo), 'First Todo')
+
+    def test_api_listview(self): # new
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+
+    def test_api_detailview(self): # new
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}),
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "First Todo")
